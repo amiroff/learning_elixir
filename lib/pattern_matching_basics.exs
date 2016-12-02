139ecfcc7eef833857d9577ef7822c15577cf912
Metin Emiroglu <amiroff@gmail.com>
2016-12-02 15:06:44 +0200

    Add binding per match examples

--- lib/pattern_matching_basics.exs
+++ lib/pattern_matching_basics.exs
@@ -10,3 +10,20 @@ list = [1, 2, 3]
 
 [a] = [[1, 2, 3]] # a=[1, 2, 3]
 [[a]] = [ [ 1, 2, 3 ] ] # does not match
+
+[a, a] = [1, 1] # matches, a=1
+[b, b] = [1, 2] # does not mach, variables can only bind once per match
+
+a = 1
+[1, a, 3] = [1, 2, 3] # a is now 2. Variable can be bount to a new value on a subsequent match
+
+a = 1
+a = 2 # a is now 2
+^a = 1 # by using pin operator we can use the exicting value. This will not match and a will not change
+
+a = 2
+[a, 2] = [1, 2] # matches, will reassign a to 1
+[^a, 2] = [2, 2] # will not match because ^a is already 1
+
+^a = 1
+^a = 2 - a # matches, a is still 1 (2-1)
