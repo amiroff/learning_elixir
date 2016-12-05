f4721d6262a51779fd80b833cdd80b744db88b63
Metin Emiroglu <amiroff@gmail.com>
2016-12-05 17:25:28 +0200

    Add multiple function bodies example

--- lib/anonymous_functions.exs
+++ lib/anonymous_functions.exs
@@ -14,3 +14,12 @@ greet.() #parentheses are required even if no arguments are passed
 # We can use pattern matching with anon functions
 swap = fn {a, b} -> {b, a} end
 swap.({6, 8}) # {8, 6}
+
+# One function, multiple bodies are possible
+handle_open = fn
+    {:ok, file} -> "Successfully read data"
+    {_,  error} -> "Error reading data"
+end
+
+handle_open.(File.open("hello.exs")) # Successfully read data
+handle_open.(File.open("404.exs")) # Error reading data
