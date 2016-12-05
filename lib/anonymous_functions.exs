2dc64467b67c699111f536015c6ddcfea40d2639
Metin Emiroglu <amiroff@gmail.com>
2016-12-05 16:32:28 +0200

    Add information about anonymous functions

---
+++ lib/anonymous_functions.exs
@@ -0,0 +1,12 @@
+# simplest function
+sum = fn (a, b) -> a + b end
+# invoking the above anonymous function
+sum.(1, 2)
+
+# We can omit parentheses in a function definition
+f1 = fn a, b -> a * b end
+f1.(5, 6)
+
+# No arguments function
+greet = fn -> IO.puts "Hello" end
+greet.() #parentheses are required even if no arguments are passed
