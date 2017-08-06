126c016c9d6189aaa80990bc328d046fbffb453f
Metin Emiroğlu <amiroff@gmail.com>
2017-08-06 16:26:06 +0300

    Named function and basic recursion examples

--- lib/named_functions_and_modules.exs
+++ lib/named_functions_and_modules.exs
@@ -36,14 +36,14 @@ def func(p1, p2 \\ 2, p3 \\ 3, p4) do
 end
 
 # number of params we are passing 2
-func("a", "b") # => ["a", 2, 3, "b"]
+Times.func("a", "b") # => ["a", 2, 3, "b"]
 
 # number of params we are passing 3
 # Elixir uses the excess to override the default values of some or all params
-func("a", "b", "c") # => ["a", "b", 3, "c"]
+Times.func("a", "b", "c") # => ["a", "b", 3, "c"]
 
 # number of params we are passing 4
-func("a", "b", "c", "d") # => ["a", "b", "c", "d"]
+Times.func("a", "b", "c", "d") # => ["a", "b", "c", "d"]
 
 # we can define private functions
 # these can be called within only the module that declares them
@@ -51,7 +51,13 @@ defp private_fun do
     "Som private text"
 end
 
-SomeModule.private_fun # => function cannot be called outside of module
+Times.private_fun # => function cannot be called outside of module
+
+# Example: sum(n) uses recursion to calculate the sum of the integers from 1 to n
+defmodule Math do
+    def sum(0), do: 0
+    def sum(n), do: n + sum(n-1)
+end
 
 # The pipe |> operator takes the result of the expressin to its left and
 # inserts it as the first parameter of the function invocation to its right
